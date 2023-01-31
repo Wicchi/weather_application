e34d974923a78b376f71c711b58fddac597c9048
Wicchi <hernot1995@gmail.com>
2023-01-31 23:40:30 +0100

    Deleted debuggers, new features.

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var time = document.querySelector('.time');
 var temp = document.querySelector('.temp');
 var winds = document.querySelector('.winds');
 var winddir = document.querySelector('.winddir');
-var chart = document.querySelector('.chart');
 import Chart from 'chart.js/auto';
 import {Map as myMap} from 'ol/index.js';
 import {Feature, Overlay, View} from 'ol/index.js';
@@ -103,23 +102,19 @@ createMap(50.07, 14.44);
 
 let cache = {};
 async function getData(url){
-    let result = "";
     if(cache[url] !== undefined) return cache[url].value;
-
     await fetch(url)
     .then(response => response.json())
     .then(json => cache[url] = {coordinate: new Date(), value: json});
-
     return cache[url].value;
 }
-
 
 setInterval(function (){
     if(Object.keys(cache).length > 0){
         let currentTime = new Date();
         Object.keys(cache).forEach(key => {
-            let seconds = currentTime - cache[key].time;
-            if(seconds > 10000){
+            let milliseconds = currentTime - new Date(cache[key].coordinate);
+            if(milliseconds > 300000){
                 delete cache[key];
                 console.log(`${key}'s cache deleted`)
             }
@@ -127,27 +122,30 @@ setInterval(function (){
     }
 }, 3000);
 
-const urlconsctructor = function(){
-    let checkedElements = document.querySelectorAll('.filled-in:checked');
-    let checkedline = [];
+const urlconsctructor = function(checkedElements){
+    let checkedline = []; 
     for (let i of checkedElements){
         checkedline.push(i.value)
-    }
-    Array.prototype.map.call(checkedline, s => s).toString();       
-    let url = 'https://api.open-meteo.com/v1/forecast?latitude='+latValue.value+'&longitude='+lonValue.value+'&hourly='+checkedline+'&timeformat=unixtime&current_weather=true&timezone=Europe%2FBerlin&contentType=json&past_days=27'
+    }      
+    let url = 'https://api.open-meteo.com/v1/forecast?latitude='+latValue.value+'&longitude='+lonValue.value+'&hourly='+checkedline.toLocaleString()+'&timeformat=unixtime&current_weather=true&timezone=Europe%2FBerlin&contentType=json&past_days=27'
     return url;
 }
 
 
 
 const plotchar = function(data, element, labels){
+    let arealabel = element + "_chart";
+    let chartStatus = Chart.getChart(arealabel);
+    if (chartStatus != undefined) {
+        chartStatus.destroy();
+    }
     let ctx = element + "_chart"
     new Chart(ctx, {
         type: "line",
         data: {
         labels: labels,
         datasets: [{
-            label: [`${element}`] +data['hourly_units'][`${element}`],
+            label: [`${element}`] + " " + data['hourly_units'][`${element}`],
             borderColor:  '#612a2a',
             tension: 0.1,
             data: data['hourly'][`${element}`],
@@ -159,27 +157,23 @@ const plotchar = function(data, element, labels){
 
 
 button.addEventListener('click', function(){
-    getData(urlconsctructor())
+    var checkedElements = document.querySelectorAll('.filled-in:checked');
+    getData(urlconsctructor(checkedElements))
         .then(data => {
             var timeValue = new Date(data['current_weather']['time'] * 1000);
             var tempValue = data['current_weather']['temperature']+" °C";
             var windsValue = data['current_weather']['windspeed'] + " kmh";
             var winddirValue = data['current_weather']['winddirection'] + " °";
-            let checkedElements = document.querySelectorAll('.filled-in:checked');
             if (checkedElements){
                 var alltimeValue = data['hourly']['time'];
                 for (var i = 0; i < alltimeValue.length; i++){
                     var newDateFormat = new Date(alltimeValue[i] * 1000);
                     alltimeValue[i] = newDateFormat.getDate()+'/'+newDateFormat.getMonth()+1+'/'+newDateFormat.getFullYear()+' - '+newDateFormat.getHours()+'h';
                 };
-                debugger;
-
                 for (let a of checkedElements){
                     a = a.value
-                    debugger;
                     plotchar(data, a, alltimeValue);
-                };
-                           
+                };                           
             }
 
             time.innerHTML = 'Current Time: ' + timeValue;
